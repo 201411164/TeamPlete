431c9e24ff007a944f05e39432e6330fb3e6603c
sungyejin <yegene010@gmail.com>
2019-12-12 12:05:34 +0900

    1212_yejin

--- teamplete/src/main/java/kr/co/teamplete/controller/TeamController.java
+++ teamplete/src/main/java/kr/co/teamplete/controller/TeamController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.teamplete.deadline.Deadline;
 import kr.co.teamplete.dto.ChargeVO;
 import kr.co.teamplete.dto.MemberVO;
 import kr.co.teamplete.dto.TaskFileVO;
@@ -86,7 +87,7 @@ public class TeamController {
 		mav.setViewName("team/team");
 		mav.addObject("teamList", teamList);
 		for(int i=0; i<teamList.size(); i++) {
-			deadline.add(deadline(teamList.get(i).getDeadline()));
+			deadline.add(Deadline.deadline(teamList.get(i).getDeadline()));
 			teamMemberList.add(service.selectAllMembers(teamList.get(i).getTeamId()));
 //			System.out.println(deadline(teamList.get(i)));
 		}
@@ -96,37 +97,37 @@ public class TeamController {
 		return mav;
 	}
 
-	// 남은 제출 기한 구하기
-	public String deadline(String deadlineStr) {
-		String deadline = deadlineStr;
-
-		if (deadline != null) {
-			String[] words = deadline.split("-");
-			try {
-				Calendar today = Calendar.getInstance(); // 오늘 날짜
-//				System.out.println("오늘날짜: " + today);
-				Calendar dday = Calendar.getInstance();
-
-				dday.set(Integer.parseInt(words[0]), Integer.parseInt(words[1]) - 1, Integer.parseInt(words[2]));
-//				System.out.println("dday 날짜: " + dday);
-
-				long calDay = (dday.getTimeInMillis() - today.getTimeInMillis()) / 86400000;
-				long calDay2= (today.getTimeInMillis() - dday.getTimeInMillis()) / 86400000;
-
-				if (calDay < 0)
-					
-					return   Long.toString(calDay2) + "일 초과";
-				else if (calDay == 0)
-					return "오늘";
-				else
-					return Long.toString(calDay) + "일 남음";
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return "마감기한 없음";
-	}
+//	// 남은 제출 기한 구하기
+//	public String deadline(String deadlineStr) {
+//		String deadline = deadlineStr;
+//
+//		if (deadline != null) {
+//			String[] words = deadline.split("-");
+//			try {
+//				Calendar today = Calendar.getInstance(); // 오늘 날짜
+////				System.out.println("오늘날짜: " + today);
+//				Calendar dday = Calendar.getInstance();
+//
+//				dday.set(Integer.parseInt(words[0]), Integer.parseInt(words[1]) - 1, Integer.parseInt(words[2]));
+////				System.out.println("dday 날짜: " + dday);
+//
+//				long calDay = (dday.getTimeInMillis() - today.getTimeInMillis()) / 86400000;
+//				long calDay2= (today.getTimeInMillis() - dday.getTimeInMillis()) / 86400000;
+//
+//				if (calDay < 0)
+//					
+//					return   Long.toString(calDay2) + "일 초과";
+//				else if (calDay == 0)
+//					return "오늘";
+//				else
+//					return Long.toString(calDay) + "일 남음";
+//
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return "마감기한 없음";
+//	}
 	
 	
 	// 상세 팀 조회 (태스크 조회)
@@ -148,7 +149,7 @@ public class TeamController {
 		
 		for(int i=0; i<taskList.size(); i++) {
 			taskFileList.add(taskService.selectAllTaskFileS(taskList.get(i).getTaskId()));
-			deadline.add(deadline(taskList.get(i).getDeadline()));
+			deadline.add(Deadline.deadline(taskList.get(i).getDeadline()));
 			chargeMembers.add(taskService.selectAllsubmitS(taskList.get(i).getTaskId()));
 			submitN.add(taskService.selectNsubmitS(taskList.get(i).getTaskId()));
 		}
