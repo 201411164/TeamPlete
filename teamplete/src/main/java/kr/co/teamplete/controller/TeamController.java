bd81f426aa9bd264c2372d20323321e153c22176
201411164 <37547603+201411164@users.noreply.github.com>
2019-12-23 01:14:42 +0900

    TeamSearch updated

--- teamplete/src/main/java/kr/co/teamplete/controller/TeamController.java
+++ teamplete/src/main/java/kr/co/teamplete/controller/TeamController.java
@@ -253,13 +253,28 @@ public class TeamController {
 			allTeamMembers.add(service.selectAllMembers(searchTeam.getTeamId()));
 		}
 		
+		List<String> updateTime = new ArrayList<>();
+
 		ModelAndView mav = new ModelAndView();
+		for (int i = 0; i < searchTeamList.size(); i++) {
+			if (searchTeamList.get(i).getTaskLatest() != null && searchTeamList.get(i).getBoardLatest() != null) {
+				if (UpdateTime.calcLatest(searchTeamList.get(i).getTaskLatest(), searchTeamList.get(i).getBoardLatest()) >= 0) {
+					updateTime.add(UpdateTime.updateTime(searchTeamList.get(i).getTaskLatest()));
+				} else
+					updateTime.add(UpdateTime.updateTime(searchTeamList.get(i).getBoardLatest()));
+			} else if (searchTeamList.get(i).getTaskLatest() != null && searchTeamList.get(i).getBoardLatest() == null) {
+				updateTime.add(UpdateTime.updateTime(searchTeamList.get(i).getTaskLatest()));
+			} else
+				updateTime.add("업데이트 없음");
+		}
+		
+		
 		
 		mav.setViewName("team/searchResultTeam");
 		
 		map.put("searchTeamList", searchTeamList);
 		map.put("allTeamMembers", allTeamMembers);
-		
+		map.put("updateTime", updateTime);
 		mav.addAllObjects(map);
 		
 		return mav;
